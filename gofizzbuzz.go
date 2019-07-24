b2f1834efab61a6be58cf0ef86c14e06e83e9cc1
unknown <alainpoirier@hotmail.com>
2019-07-24 09:03:53 -0500

    first commit

---
+++ gofizzbuzz.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+import "strconv"
+
+func main() {
+	var result = ""
+    for i := 1; i <= 100; i++ {
+    	var fb = false
+    	if i %3 == 0 {
+        	result += " Fizz"
+        	fb = true
+    	}
+    	if i %5 == 0 {
+        	result += " Buzz"
+        	fb = true
+    	}
+    	if fb != true {
+    		result += " "
+    		result += strconv.Itoa(i)
+    	}
+        result += ","
+    }
+    fmt.Println(result)
+}
